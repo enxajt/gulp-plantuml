01e16bb667ac99bd61064e32a737c1fc4826326b
root <root@ubuntu-xenial.localdomain>
2017-01-27 15:25:15 +0000

    add ejs all png

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,10 @@ var exec = require('gulp-exec');
 var ejs = require('gulp-ejs');
 var rename = require('gulp-rename');
 var cached = require('gulp-cached');
-//var plumber = require('gulp-plumber');
+var plumber = require('gulp-plumber');
 var fs = require('fs');
 var jsonTransform = require('gulp-json-transform');
+var notify = require('gulp-notify');
 
 var _path = {
 	src : './src',
@@ -20,7 +21,7 @@ var _path = {
 };
 
 gulp.task('webserver',function() {
-  gulp.src(_path.dst)
+  gulp.src('./')
     .pipe(webserver({
       livereload: true,
       host: '0.0.0.0',
@@ -30,33 +31,50 @@ gulp.task('webserver',function() {
     }));
 });
 
-gulp.task('ejs', function() {
-	var json = JSON.parse(fs.readFileSync(_path.last));
-	gulp.src(["./ejs/**/*.ejs","!./ejs/template/*.ejs","!./ejs/var/*.json"])
-	//.pipe(plumber())
-	.pipe(ejs(json))
-	.pipe(rename({extname: '.html'}))
-	.pipe(gulp.dest(_path.dst))
-	.pipe(print(function(filepath) {
-		return "ejs: " + filepath;
-	}));
+gulp.task('last', function() {
+  var last = JSON.parse(fs.readFileSync(_path.last));
+  gulp.src([
+      "./ejs/**/*.ejs",
+      "!./ejs/template/*.ejs",
+      "!./ejs/var/*.json"])
+      .pipe(ejs({
+	      jsonData: last 
+      }))
+    .pipe(rename('last.html'))
+    .pipe(gulp.dest(_path.dst));
+});
+
+gulp.task('all', function() {
+  var pages = JSON.parse(fs.readFileSync(_path._var+"/pages.json")).pages;
+  for (var i=0; i<pages.length; i++) {
+    console.log(pages[i]);
+    gulp.src([
+        "./ejs/**/*.ejs",
+        "!./ejs/template/*.ejs",
+        "!./ejs/var/*.json"])
+      .pipe(ejs({
+	      jsonData: pages[i]
+      }))
+      .pipe(rename(pages[i].id+'.html'))
+      .pipe(gulp.dest(_path.dst));
+  } 
 });
 
 gulp.task('img', function() {
-	gulp.src(_path.src+'/*.png')
-	  .pipe(cached('img'))
-	  //.pipe(plumber())
-	  .pipe(tap(function(file, t) {
-	    var img_name = path.basename(file.path);
-	    gulp.src(_path.last)
-	      .pipe(jsonTransform(function(data, file) {
-		return {
-			imgName: img_name 
-		};
-	        return 'imgName: '+file.path;
-              }))
-	      .pipe(gulp.dest(_path._var));
-	  }));
+  gulp.src(_path.src+'/*.png')
+    .pipe(cached('img'))
+    .pipe(tap(function(file, t) {
+      var img_name = path.basename(file.path);
+      gulp.src(_path.last)
+        .pipe(jsonTransform(function(data, file) {
+  	  return {
+		id: img_name,
+		title: img_name,
+  	  	img: img_name 
+  	  };
+        }))
+        .pipe(gulp.dest(_path._var));
+    }));
 });
 
 gulp.task('plantuml', function() {
@@ -74,10 +92,10 @@ gulp.task('plantuml', function() {
 });
 
 gulp.task('watch', function() {
-	gulp.watch([_path.last],['ejs']);
+	gulp.watch([_path.last],['last']);
 	gulp.watch([_path.dst+'/*.png'],['img']);
 	gulp.watch([_path.src+'/*.pu'],['plantuml']);
 	gulp.src('gulpfile.js');
 });
 
-gulp.task('default', ['watch', 'webserver','plantuml','img','ejs']);
+gulp.task('default', ['watch', 'webserver','plantuml','all']);
